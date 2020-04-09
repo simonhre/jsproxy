6dc98e3372589c59cb9cdd711e7039ea9cbbab45
simonhre <63359191+simonhre@users.noreply.github.com>
2020-04-09 13:02:02 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -36,7 +36,7 @@ jsproxy_config({
     'cfworker': {
       label: '',
       hidden: true,
-      lines: { 'holysky.simonhre.workers.dev'
+      lines: { 
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
 
